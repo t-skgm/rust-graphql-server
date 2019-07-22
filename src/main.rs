6e46ebcd33e6121adbad66a6ac631e53c83d2e36
t-skgm <skgm0301@gmail.com>
2019-07-22 14:28:05 +0900

    add router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+static SERVER_ADDR: &str = "localhost:3000";
+
+fn root(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello, world")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+    router.get("/", root, "root");
+
+    let _server = Iron::new(router).http(SERVER_ADDR).unwrap();
+    println!("Starting server on http://{}", SERVER_ADDR)
 }
